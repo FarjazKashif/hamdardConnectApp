5829a5eb5e3b7b746d29e966f18749a23dcc89c8
Farjaz Kashif <farjazkashif01@gmail.com>
2026-02-09 23:31:14 +0500

    Initial Commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
-const Home = () => {
+const Page = () => {
   const { isSignedIn } = useAuth()
     if (isSignedIn) {
+      // Redirect to Home
         return <Redirect href="/(root)/(tabs)/home" />
     }
   return <Redirect href="/(auth)/welcome" />
 }
 
-export default Home;
+export default Page;
